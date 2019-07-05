853e24fe65349048e5c3431025a9d7875711e72b
Joakim Bygdell <joakim.bygdell@nbis.se>
2019-07-05 08:29:54 +0200

    Add TLS to cega-mq
    This adds the TLS option to cega-mq while retaining the option to use non secured connections for testing purposes.

--- ega-charts/cega/conf/cega-mq.sh
+++ ega-charts/cega/conf/cega-mq.sh
@@ -8,10 +8,10 @@ rabbitmq-plugins enable --offline rabbitmq_federation_management
 rabbitmq-plugins enable --offline rabbitmq_shovel
 rabbitmq-plugins enable --offline rabbitmq_shovel_management
 
-cp --remove-destination /temp/rabbitmq.config /etc/rabbitmq/rabbitmq.config
+cp --remove-destination /temp/rabbitmq.conf /etc/rabbitmq/rabbitmq.conf
 cp --remove-destination /temp/defs.json /etc/rabbitmq/defs.json
-chown rabbitmq:rabbitmq /etc/rabbitmq/rabbitmq.config
-chmod 640 /etc/rabbitmq/rabbitmq.config
+chown rabbitmq:rabbitmq /etc/rabbitmq/rabbitmq.conf
+chmod 640 /etc/rabbitmq/rabbitmq.conf
 chown rabbitmq:rabbitmq /etc/rabbitmq/defs.json
 chmod 640 /etc/rabbitmq/defs.json
 
