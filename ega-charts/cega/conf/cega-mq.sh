c617c7105d5063b2fe2952b1e56e414be137549d
Joakim Bygdell <joakim.bygdell@nbis.se>
2019-08-29 08:17:26 +0200

    Simplify cega-mq configurations
    Since we can tell rabbitmq where the config file is located we don't need to use the entrypoint script to copy the files from one location to another.

--- ega-charts/cega/conf/cega-mq.sh
+++ ega-charts/cega/conf/cega-mq.sh
@@ -8,11 +8,4 @@ rabbitmq-plugins enable --offline rabbitmq_federation_management
 rabbitmq-plugins enable --offline rabbitmq_shovel
 rabbitmq-plugins enable --offline rabbitmq_shovel_management
 
-cp --remove-destination /temp/rabbitmq.conf /etc/rabbitmq/rabbitmq.conf
-cp --remove-destination /temp/defs.json /etc/rabbitmq/defs.json
-chown rabbitmq:rabbitmq /etc/rabbitmq/rabbitmq.conf
-chmod 640 /etc/rabbitmq/rabbitmq.conf
-chown rabbitmq:rabbitmq /etc/rabbitmq/defs.json
-chmod 640 /etc/rabbitmq/defs.json
-
 exec rabbitmq-server
